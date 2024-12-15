accebf53b878b44303d60fd0dc582fe8c9fe138d
Ravi Yadav <yadavr74839@gmail.com>
2024-12-16 01:52:18 +0530

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -14,25 +14,25 @@ export default function About() {
           </div>
           <div className="md:pl-10 md:pt-0 pt-10 flex flex-col gap-5">
             <p>
-              Welcome to Prescripto, your trusted partner in managing your
-              healthcare needs conveniently and efficiently. At Prescripto, we
+              Welcome to DoctApp, your trusted partner in managing your
+              healthcare needs conveniently and efficiently. At DoctApp, we
               understand the challenges individuals face when it comes to
               scheduling doctor appointments and managing their health records.
             </p>
 
             <p>
-              Prescripto is committed to excellence in healthcare technology. We
+            DoctApp is committed to excellence in healthcare technology. We
               continuously strive to enhance our platform, integrating the
               latest advancements to improve user experience and deliver
               superior service. Whether you're booking your first appointment or
-              managing ongoing care, Prescripto is here to support you every
+              managing ongoing care, DoctApp is here to support you every
               step of the way.
             </p>
 
             <p className="text-black font-bold">Our Vision</p>
 
             <p>
-              Our vision at Prescripto is to create a seamless healthcare
+              Our vision at DoctApp is to create a seamless healthcare
               experience for every user. We aim to bridge the gap between
               patients and healthcare providers, making it easier for you to
               access the care you need, when you need it.
